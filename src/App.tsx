7926d791c26cb7c5d9da829df63aa03dfb1831d4
Dayvon Allen <dayvon@107-144-156-227.biz.spectrum.com>
2020-02-07 19:01:36 -0500

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { LoginComponent } from './components/login-component/LoginComponent';
+import { HomeComponent } from './components/home-component/HomeComponent';
+import { RegisterComponent } from './components/register-component/RegisterComponent';
+
+const App: React.FC = () => {
+  return (
+    <div className="App">
+      <Router>
+        <Switch>
+        <Route path="/login" component={LoginComponent} />
+        <Route path="/home" component={HomeComponent} />
+        <Route path="/register" component={RegisterComponent} />
+        </Switch>
+      </Router>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/login-component/LoginComponent.tsx
@@ -0,0 +1,48 @@
+import React, { SyntheticEvent } from 'react';
+
+// interface ILoginState {
+//     username: string
+//     password: string
+// }
+
+// interface ILoginProps {
+//     updateCurrentUser: (u:string, p:string) => void
+//     loginMessage: string
+// }
+
+export class LoginComponent extends React.Component{
+    // constructor(props: any) {
+    //     super(props)
+    //     this.state = {
+    //         username: '',
+    //         password: ''
+    //     }
+    // }
+
+    // updateUsername = (event: any) => {
+    //     this.setState({
+    //         ...this.state,
+    //         username: event.target.value
+    //     })
+    // }
+
+    // updatePassword = (event: any) => {
+    //     this.setState({
+    //         ...this.state,
+    //         password: event.target.value
+    //     })
+    // }
+
+    // submitLogin = async (event: SyntheticEvent) => {
+    //     event.preventDefault()
+    //     this.props.updateCurrentUser(this.state.username, this.state,password)
+    // }
+    
+    render(){
+        return(
+            <div>
+                <h1>TEST</h1>
+            </div>
+        )
+    }
+}
